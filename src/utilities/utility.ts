183eccb9cf4ce378efb09fe14c13603c811c5556
jitiendran <jitiendranjiji2000@gmail.com>
2023-05-28 13:53:54 +0530

    refactor(core)!: remove create module from arclix

--- src/utilities/utility.ts
+++ src/utilities/utility.ts
@@ -4,11 +4,10 @@ import { createSpinner } from 'nanospinner';
 const log = console.log;
 const spinner = createSpinner();
 const emptyLine = () => log();
-const errorLog = (log: string) => chalk.red(log);
 const primaryChalk = chalk.hex('#09d3ac');
 
 const convertToTitleCase = (componentName: string) =>
   componentName.slice(0, 1).toUpperCase() +
   componentName.slice(1, componentName.length);
 
-export { log, spinner, emptyLine, errorLog, primaryChalk, convertToTitleCase };
+export { log, spinner, emptyLine, primaryChalk, convertToTitleCase };
